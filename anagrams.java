bb7dbcf7fcaf3e9885007a09f2f73347510431be
prabhjot023 <44260384+prabhjot023@users.noreply.github.com>
2020-01-17 20:11:21 +0530

    Create anagrams.java

---
+++ anagrams.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class anagram_ques {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1,s2;
+		s1=sc.next();
+		s2=sc.next();
+		s1=s1.toLowerCase();
+		s2=s2.toLowerCase();
+		char ch[]=s1.toCharArray();
+		char ch1[]=s2.toCharArray();
+		Arrays.sort(ch);
+		Arrays.sort(ch1);
+		String s=new String(ch);
+		String ss=new String(ch1);
+		if(s.equalsIgnoreCase(ss))
+				{
+			System.out.print("Anagrams");
+				}
+		else{
+			
+			System.out.print("Not Anagarams");
+		}
+
+	}
+
+}
